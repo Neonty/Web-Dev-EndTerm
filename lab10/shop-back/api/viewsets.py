561dbfdc182ec50d6987dafb2cc427740ef01663
kulmuratovva <anelkulmuratovaaa@gmail.com>
2026-04-05 22:05:11 +0500

    Add lab10

---
+++ lab10/shop-back/api/viewsets.py
@@ -0,0 +1,33 @@
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    @action(detail=True, methods=['get'])
+    def products(self, request, pk=None):
+        category = self.get_object()
+        products = category.products.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    @action(detail=False, methods=['delete'])
+    def clear(self, request):
+        inactive_products = Product.objects.filter(is_active=False)
+        count = inactive_products.count()    
+        inactive_products.delete() 
+
+        return Response(
+            {"deleted": count}
+        )
